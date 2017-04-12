c6d8da823e7cb2f33e08748102c7b7df2ef5a0ae
Konekoya <tank158r5155@yahoo.com.tw>
2017-04-12 09:43:34 +0800

    Update

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
-export default class NewPost extends Component {
+class NewPost extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
     return (
-      <div>
-        Create Form
-      </div>
+      <form onSubmit={ handleSubmit }>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input type="text" className="form-control" { ...title } />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="">Categories</label>
+          <input type="text" className="form-control" { ...categories } />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="">Content</label>
+          <textarea className="form-control" { ...content } />
+        </div>
+
+        <button type="submit" className="btn btn-primary">submit</button>
+      </form>
     );
   }
 }
+
+export default reduxForm({
+  form: 'NewPostForm',
+  fields: ['title', 'categories', 'content']
+})(NewPost);
--- src/components/PostHome.js
+++ src/components/PostHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import { Link } from 'react-router';
 
 class PostHome extends Component {
   componentWillMount() {
@@ -9,6 +10,11 @@ class PostHome extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to='/posts/new' className='btn btn-primary'>
+            Add a Post
+          </Link>
+        </div>
         <div>List of blog posts.</div>
       </div>
     );
