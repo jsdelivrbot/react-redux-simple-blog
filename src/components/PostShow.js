f6aefe1471cce43a95531871d6394283be5f9002
Konekoya <tank158r5155@yahoo.com.tw>
2017-04-24 09:20:30 +0800

    Finish the course

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,11 +1,45 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { fetchPost, deletePost } from '../actions';
 
-export default class PostShow extends Component {
+class PostShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+  onDeleteClick = () => {
+    this.props.deletePost(this.props.params.id)
+      .then(() => { this.context.router.push('/') });
+  }
   render() {
+    const { post } = this.props;
+    if (!post) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        Show page { this.props.params.id }
+        <Link to='/'>Back to Index</Link>
+        <button
+          onClick={ this.onDeleteClick }
+          className='btn btn-danger pull-xs-right'>
+          Delete Post
+        </button>
+        <h3>{ post.title }</h3>
+        <h6>{ post.categories }</h6>
+        <p>{ post.content }</p>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  console.log(state);
+  return {
+    post: state.posts.post
+  }
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 const initialState = {
   all: [],
   post: null
@@ -6,6 +6,11 @@ const initialState = {
 
 export default function PostsReducer(state = initialState, action) {
   switch (action.type) {
+  case FETCH_POST:
+    return {
+      ...state,
+      post: action.payload.data
+    }
   case FETCH_POSTS:
     return {
       ...state,
